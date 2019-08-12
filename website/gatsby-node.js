7a1d561b03b21e51998fc91e66d1137aea6f0d4b
Bruno Alla <alla.brunoo@gmail.com>
2019-08-12 23:07:04 +0100

    Create pages from Sanity content

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -3,3 +3,44 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`)
+
+// Create a page for each markdown file for photos
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const result = await graphql(`
+    {
+      allSanityGallery(sort: { fields: date, order: DESC }) {
+        edges {
+          node {
+            id
+            title
+            slug {
+              current
+            }
+            date(formatString: "DD MMMM YYYY", locale: "fr-FR")
+          }
+        }
+      }
+    }
+  `)
+
+  // In case of errors, stop here
+  if (result.errors) {
+    reporter.panicOnBuild(`🚨 Error in query for pages 🚨`)
+    return
+  }
+
+  // Process result & create pages
+  result.data.allSanityGallery.edges.forEach(({ node }) => {
+    actions.createPage({
+      path: `/photos/${node.slug.current}/`,
+      component: path.resolve(`./src/templates/gallery-page.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        id: node.id,
+        location: `/photos/${node.slug.current}/`,
+      },
+    })
+  })
+}
--- website/src/templates/gallery-page.js
+++ website/src/templates/gallery-page.js
@@ -3,49 +3,40 @@ import React from 'react'
 import Layout from '../components/layout'
 import Gallery from '@browniebroke/gatsby-image-gallery'
 
-const GalleryPage = ({ data }) => {
-  const post = data.post
-  const images = data.images.edges
-  const fullSize = images.map(imageNode => imageNode.node.full.fluid.src)
-  const thumbs = images.map(imageNode => imageNode.node.thumb.fluid)
+const GalleryPage = ({ data, pageContext }) => {
+  const page = data.sanityGallery
+  const images = page.photos
+  const fullSize = images.map(imageNode => imageNode.asset.full.src)
+  const thumbs = images.map(imageNode => imageNode.asset.thumb)
   return (
-    <Layout>
+    <Layout location={pageContext.location}>
       <div className="my-4">
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <h1>{page.title}</h1>
         <Gallery images={fullSize} thumbs={thumbs} colClass="col-4 col-md-3" />
       </div>
     </Layout>
   )
 }
 
-// export const pageQuery = graphql`
-//   query GalleryData($imagePath: String!, $slug: String!) {
-//     images: allFile(filter: { relativeDirectory: { eq: $imagePath } }) {
-//       edges {
-//         node {
-//           id
-//           relativeDirectory
-//           thumb: childImageSharp {
-//             fluid(maxWidth: 270, maxHeight: 270) {
-//               ...GatsbyImageSharpFluid
-//             }
-//           }
-//           full: childImageSharp {
-//             fluid(maxWidth: 1024) {
-//               ...GatsbyImageSharpFluid
-//             }
-//           }
-//         }
-//       }
-//     }
-//     post: markdownRemark(fields: { slug: { eq: $slug } }) {
-//       html
-//       frontmatter {
-//         title
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query GalleryData($id: String!) {
+    sanityGallery(id: { eq: $id }) {
+      title
+      slug {
+        current
+      }
+      photos {
+        asset {
+          thumb: fluid(maxWidth: 270, maxHeight: 270) {
+            ...GatsbySanityImageFluid
+          }
+          full: fluid(maxWidth: 1024) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`
 
 export default GalleryPage
