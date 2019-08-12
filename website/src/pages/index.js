38f53f68b28ddd187d841ec640f040c0cd5961c0
Bruno Alla <alla.brunoo@gmail.com>
2019-08-12 23:49:16 +0100

    Add image for homepage

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-const IndexPage = ({ location, data }) => (
-  <Layout location={location}>
-    <div className="container">
-      <h1>Les Capsules</h1>
-      <p>Bienvenue sur notre Site</p>
-    </div>
-  </Layout>
-)
+const IndexPage = ({ location, data }) => {
+  return (
+    <Layout location={location}>
+      <div>
+        <Img fluid={data.file.childImageSharp.fluid} />
+      </div>
+      <div className="container">
+        <h1>Les Capsules</h1>
+        <p>Bienvenue sur notre Site</p>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query HomeImage {
+    file(relativePath: { eq: "images/comite-grand.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1200, maxHeight: 800) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
