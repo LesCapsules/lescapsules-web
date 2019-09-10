4f15ab4a3c2dedc23e9041b7f79c4463f54120d6
Bruno Alla <alla.brunoo@gmail.com>
2019-09-10 22:24:15 +0100

    Change footer background color

--- website/src/components/footer.js
+++ website/src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer className="footer mt-auto bg-secondary py-5">
+  <footer className="footer mt-auto bg-light py-5">
     <div className="container">
       <p className="text-center">Les Capsules 2019</p>
     </div>
