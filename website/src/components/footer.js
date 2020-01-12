702e5b5200de46567f7f6f04be753ce6c00dfeea
Bruno Alla <alla.brunoo@gmail.com>
2020-01-12 20:01:33 +0000

    Tweaks in footer

--- website/src/components/footer.js
+++ website/src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebook } from 'react-icons/fa'
+import { FaFacebook, FaEnvelope } from 'react-icons/fa'
 
 import Container from './container'
 import Row from './row'
@@ -10,13 +10,21 @@ const Footer = () => (
     <Container>
       <Row alignItems="center">
         <div className="col-md text-center text-md-left">
-          <ExternalLink to="https://www.facebook.com/lescapsules/">
-            <FaFacebook size="3rem" />
-          </ExternalLink>
+          <ul className="list-inline">
+            <li className="list-inline-item">
+              <ExternalLink to="https://www.facebook.com/lescapsules/">
+                <FaFacebook size="3rem" />
+              </ExternalLink>
+            </li>
+            <li className="list-inline-item">
+              <ExternalLink to="mailto:lescapsules@gmail.com">
+                <FaEnvelope size="3rem" />
+              </ExternalLink>
+            </li>
+          </ul>
         </div>
         <div className="col-md text-center text-md-right">
-          © Capsules {new Date().getFullYear()} - Fait avec{' '}
-          <ExternalLink to="https://www.gatsbyjs.org/">Gatsby</ExternalLink>
+          © Capsules {new Date().getFullYear()}
         </div>
       </Row>
     </Container>
