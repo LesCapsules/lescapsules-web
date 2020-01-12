1619ecb20b361d17e37b89b5700087271a8519f1
Bruno Alla <alla.brunoo@gmail.com>
2020-01-12 19:21:22 +0000

    Fragment

--- website/src/components/profile.js
+++ website/src/components/profile.js
@@ -3,14 +3,14 @@ import Img from 'gatsby-image'
 
 const Profile = ({ name, fluidImg, favouritePlace, hobbies }) => {
   return (
-    <div>
+    <>
       <Img fluid={fluidImg} />
       <div className="m-2">
         <h4>{name}</h4>
         <p>Lieu de bandade favori: {favouritePlace}</p>
         <p>Hobbies: {hobbies}</p>
       </div>
-    </div>
+    </>
   )
 }
 
