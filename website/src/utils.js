2b0c29292b099e1e9094d86e8708b9161410cb33
Bruno Alla <alla.brunoo@gmail.com>
2020-01-15 23:20:05 +0000

    Remove special characters from the pages slug

--- website/src/utils.js
+++ website/src/utils.js
@@ -3,6 +3,7 @@ const slugify = require(`slugify`)
 exports.makeAlbumPagePath = (title, year) => {
   const slug = slugify(title, {
     lower: true,
+    remove: /[*+~.()'"!:@]/g,
   })
   return `/photos/${year}/${slug}/`
 }
