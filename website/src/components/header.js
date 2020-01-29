490f38ca5cf354a15fcc1c3d7f247a46eb0dd654
Bruno Alla <alla.brunoo@gmail.com>
2020-01-29 17:15:17 +0000

    Wrap <li> from navigation inside a <ul>

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -1,24 +1,38 @@
 import React from 'react'
+import Container from './container'
+import { Link } from 'gatsby'
+import { slide as Menu } from 'react-burger-menu'
 import { IoMdImages, IoMdVideocam, IoMdHome, IoMdPeople } from 'react-icons/io'
-import NavBav from './nav-bar'
+
 import NavItem from './nav-item'
+import NavItemList from './nav-item-list'
+import LogoCapsules from '../images/icons/lescapsules-name.svg'
 
 const Header = () => (
   <header>
-    <NavBav>
-      <NavItem to="/">
-        <IoMdHome /> Acceuil
-      </NavItem>
-      <NavItem to="/photos/">
-        <IoMdImages /> Photos
-      </NavItem>
-      <NavItem to="/videos/">
-        <IoMdVideocam /> Vidéos
-      </NavItem>
-      <NavItem to="/drink-team/">
-        <IoMdPeople /> Drink Team
-      </NavItem>
-    </NavBav>
+    <nav className="navbar navbar-expand bg-primary navbar-dark">
+      <Container>
+        <Link to="/" className="navbar-brand d-flex align-items-center">
+          <LogoCapsules />
+        </Link>
+        <Menu right>
+          <NavItemList>
+            <NavItem to="/">
+              <IoMdHome /> Acceuil
+            </NavItem>
+            <NavItem to="/photos/">
+              <IoMdImages /> Photos
+            </NavItem>
+            <NavItem to="/videos/">
+              <IoMdVideocam /> Vidéos
+            </NavItem>
+            <NavItem to="/drink-team/">
+              <IoMdPeople /> Drink Team
+            </NavItem>
+          </NavItemList>
+        </Menu>
+      </Container>
+    </nav>
   </header>
 )
 
