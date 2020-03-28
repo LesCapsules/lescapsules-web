f1a3e243507f342895e5d864734a45477878c00d
Bruno Alla <alla.brunoo@gmail.com>
2020-03-28 13:47:35 +0000

    Setup preview via Heroku

---
+++ sanity-studio/parts/dashboardConfig.js
@@ -0,0 +1,14 @@
+export default {
+  widgets: [
+    {
+      name: 'gatsby',
+      options: {
+        sites: [
+          {
+            siteUrl: 'https://capsules-preview.herokuapp.com/',
+          },
+        ],
+      },
+    },
+  ],
+}
