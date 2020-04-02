a18b532cb98a8e20a1850fbf1bac55d1ed9c5795
Bruno Alla <alla.brunoo@gmail.com>
2020-04-02 12:56:24 +0100

    Make preview works with all pages

--- sanity-studio/parts/deskStructure.js
+++ sanity-studio/parts/deskStructure.js
@@ -1,25 +1,46 @@
+import React from 'react'
 import S from '@sanity/desk-tool/structure-builder'
 import { FiEdit3 as EditIcon } from 'react-icons/fi'
 import { FiEye as PreviewIcon } from 'react-icons/fi'
-import WebPreview from '../previews/WebPreview'
+
+import { makeAlbumPagePath } from '../../website/src/utils'
 
 const previewUrl = 'https://capsules-preview.herokuapp.com'
 
-export const getDefaultDocumentNode = ({ schemaType, documentId }) => {
-  if (schemaType === 'gallery') {
-    return S.document().views([
-      S.view.form().icon(EditIcon),
-      S.view
-        .component(WebPreview)
-        .options({ previewUrl: `${previewUrl}/preview/${documentId}` })
-        .title('Web Preview')
-        .icon(PreviewIcon),
-    ])
+const getFullPreviewURL = (schemaType, document) => {
+  switch (schemaType.name) {
+    case 'gallery':
+      const date = new Date(document.displayed.date)
+      const title = document.displayed.title
+      return previewUrl + makeAlbumPagePath(title, date.getFullYear())
+    case 'member':
+      return previewUrl + '/drink-team/'
+    case 'video':
+      return previewUrl + '/videos/'
+    default:
+      return previewUrl
   }
-  return S.document().views([S.view.form()])
 }
 
-export default () =>
-  S.list()
-    .title('Content')
-    .items([...S.documentTypeListItems()])
+const WebPreview = ({ schemaType, document }) => {
+  const fullPreviewUrl = getFullPreviewURL(schemaType, document)
+  return (
+    <div>
+      <iframe
+        src={fullPreviewUrl}
+        width="100%"
+        style={{ position: 'absolute', height: '100%' }}
+        frameBorder={0}
+      />
+    </div>
+  )
+}
+
+export const getDefaultDocumentNode = () => {
+  return S.document().views([
+    S.view.form().icon(EditIcon),
+    S.view.component(WebPreview).title('Preview').icon(PreviewIcon),
+  ])
+}
+
+export default S.defaults()
