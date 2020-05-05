1c87fe529caaa4d10d0cd922e8f3ba5d04590948
Bruno Alla <alla.brunoo@gmail.com>
2020-05-05 22:55:36 +0100

    Global styles to set margin and padding

--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 import '../scss/main.scss'
 import Header from './header'
@@ -11,8 +11,28 @@ const ContentWrapper = styled.div`
   min-height: calc(100vh - 100px - 6rem);
 `
 
+const GlobalStyles = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+}
+`
+
 const Layout = ({ title, description, image, path, children }) => (
   <>
+    <GlobalStyles />
     <SEO
       title={title}
       description={description}
