683d873bb898c216b27c4a2f8f55323916b1bbab
Bruno Alla <alla.brunoo@gmail.com>
2020-05-05 19:45:34 +0100

    Migrate Profile to styled components

--- website/src/components/profile.js
+++ website/src/components/profile.js
@@ -1,30 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import { gridBreakpoints, spacings } from './constants'
+
+const ProfileStyles = styled.div`
+  box-shadow: rgba(29, 33, 41, 0.15) 0px 2px 40px 0px;
+  padding: ${spacings[2]};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: ${gridBreakpoints.sm}) {
+    flex-direction: row;
+  }
+`
 
 const ImgProfile = styled.img`
-  max-width: 100%;
+  flex: 0 1 150px;
   border-radius: 50%;
   border: 1px solid rgb(81, 85, 93);
-  padding: 3px;
+  padding: ${spacings[0]};
+`
+
+const BioStyles = styled.div`
+  margin-left: ${spacings[2]};
 `
 
 const Profile = ({ name, imgSrc, favouritePlace, hobbies }) => {
   return (
-    <div
-      className="row p-3"
-      style={{
-        boxShadow: 'rgba(29, 33, 41, 0.15) 0px 2px 40px 0px',
-      }}
-    >
-      <div style={{ width: '30%', maxWidth: '150px' }}>
-        <ImgProfile src={imgSrc} alt={name} />
-      </div>
-      <div className="col m-2">
+    <ProfileStyles>
+      <ImgProfile src={imgSrc} alt={name} />
+      <BioStyles>
         <h4>{name}</h4>
         <p>Lieu de bandade favori: {favouritePlace}</p>
         <p>Hobbies: {hobbies}</p>
-      </div>
-    </div>
+      </BioStyles>
+    </ProfileStyles>
   )
 }
 
